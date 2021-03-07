7adbf9263a52e124b46526eac5193a20d06e4d61
JaySonani <jsonani98@gmail.com>
2021-03-07 22:05:27 +0530

    Phase 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+  "assets/AssetManifest.json": "5976ad1080759647fa5c610c343c2988",
+"assets/assets/fonts/Lato-Regular.ttf": "2d36b1a925432bae7f3c53a340868c6e",
+"assets/assets/fonts/Montserrat-Regular.ttf": "ee6539921d713482b8ccd4d0d23961bb",
+"assets/FontManifest.json": "3c46f9771e1f2c4a6c54922dc6641329",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "efc331cc3689a0d613ed6c9f17f62e21",
+"assets/NOTICES": "d2e337d79f25b7a36075d894631f6345",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b05f14a12f0af7b013beebb1ebe2b5d5",
-"/": "b05f14a12f0af7b013beebb1ebe2b5d5",
-"main.dart.js": "1c2c595c18bda1f233a304efb34d653f",
+"index.html": "a894145751a7b24ad6d7c928e982ad92",
+"/": "a894145751a7b24ad6d7c928e982ad92",
+"main.dart.js": "c7d2a307293ffa110bf566ba7f30a60f",
 "manifest.json": "3febc81cc0c69eb361d856a5a43d5a4b",
 "version.json": "ffafa860161421d22de09bc543584ca3"
 };
